e912eaff94038fe2e754a38a8f3245b311410214
SheikhJaveed <SheikhJaveed@users.noreply.github.com>
2025-08-26 13:04:49 +0000

    deploy: cdad5f19857715832c8bffa121804676cd9d4334

---
+++ script.js
@@ -0,0 +1,29 @@
+const dictionary = (word) => {
+    fetch(`http://localhost:3000/dictionary?word=${word}`)
+        .then(response => response.json())
+        .then(response => {
+            if (!response.definition || response.definition.includes("No definition found")) {
+                document.getElementById('word').innerHTML = "Not Found";
+                document.getElementById('definition').innerHTML = "❌ No definition found for this word.";
+            } else {
+                document.getElementById('word').innerHTML = response.word;
+                document.getElementById('definition').innerHTML = response.definition.replace(/\d/g, "<br>$&");
+            }
+        })
+        .catch(err => console.log(err));
+};
+
+
+// Event listener for search button
+document.getElementById("searchBtn").addEventListener("click", (e) => {
+    e.preventDefault();
+    dictionary(document.querySelector("input[type='search']").value);
+});
+
+
+document.querySelector(".home-btn").addEventListener("click", (e) => {
+    e.preventDefault(); // Prevents page reload
+    document.querySelector("input[type='search']").value = ""; // Clears search input
+    document.getElementById("word").innerHTML = "Your word will appear here"; // Resets word display
+    document.getElementById("definition").innerHTML = "Welcome to the dictionary app.<br>Type your favorite word in the search bar."; // Resets definition
+});
